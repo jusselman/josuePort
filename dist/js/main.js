4856f9454e5f77e0a8272482ea84a481f4d7eeff
Joshua Usselman <usselmanjosh@gmail.com>
2018-09-10 20:59:48 -0700

    styling main portal

--- dist/js/main.js
+++ dist/js/main.js
@@ -103,3 +103,40 @@ $(window).scroll(function(){
          .end().filter("[href=\\#"+id+"]").parent().addClass("active");
    }
 });
+
+
+// SCROLL REVEAL //
+if (window.innerWidth > 769) {
+ window.sr = ScrollReveal();
+  sr.reveal('.fadeInLeft', {
+    duration: 2500,
+    origin: 'top',
+    distance: '200px'
+  });
+  sr.reveal('.fadeInTop', {
+    duration: 2500,
+    origin: 'bottom',
+    distance: '200px'
+  });
+};
+
+
+// PROFESSION FADER //
+
+var faded_i = 0;
+var faded_array = ["Web Designer", "UI/UX Designer", "Photographer", "Epicurean"];
+var faded_elem;
+faded_elem = document.getElementById('faded'); fadedSlide();
+function fadedNext() {
+  faded_i++;
+  faded_elem.style.opacity = 0;
+  if(faded_i > (faded_array.length - 1)) {
+    faded_i = 0;
+  }
+  setTimeout('fadedSlide()', 1500)
+}
+function fadedSlide() {
+  faded_elem.innerHTML = faded_array[faded_i];
+  faded_elem.style.opacity = 1;
+  setTimeout('fadedNext()',6000);
+}
